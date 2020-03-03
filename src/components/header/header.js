88eae7d0fbacd64e31a5edaa41d1d4434604fd2b
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-03 13:15:53 +0530

    Clean header component and add styles

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,12 +1,10 @@
-import { Layout, Typography } from "antd";
-import React from "react";
+import { Layout, Avatar } from 'antd';
+import React from 'react';
+import styles from './header.module.scss';
 const { Header } = Layout;
-const { Text } = Typography;
 
 export default () => (
-  <Layout>
-    <Header>
-      <Text type="danger">Header</Text>
-    </Header>
-  </Layout>
+  <Header className={styles.header}>
+    <Avatar size="large" src={require('../../assets/images/claw.jpg')} />
+  </Header>
 );
