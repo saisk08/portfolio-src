a0a5a85533c99a5a9b901dc8260523d518d6edbc
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-03-03 13:14:58 +0530

    Add title and typography plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,12 +5,23 @@
  */
 
 module.exports = {
+  siteMetadata: {
+    title: `@somanath08`,
+    // siteUrl: `https://www.gatsbyjs.org`,
+    description: `Sai Somanath Komanduri's portfolio webiste in react`,
+  },
   plugins: [
     `gatsby-plugin-sass`,
     {
       resolve: `gatsby-plugin-less`,
       options: {
         javascriptEnabled: true,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-typography`,
+      options: {
+        pathToConfigModule: `src/utils/typography`,
       },
     },
   ],
