41874c5745cea894a3705855cce2eb830b074aba
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-03 18:34:16 +0530

    Remove all components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,12 @@
 import React from 'react';
 import { Layout, SEO } from 'components/common';
 import { Intro, Skills, Contact, Projects } from 'components/landing';
+import { Footer } from 'components/theme';
 
 export default () => (
   <Layout>
     <SEO />
     <Intro />
-    <Projects />
-    <Skills />
-    <Contact />
+    <Footer />
   </Layout>
 );
