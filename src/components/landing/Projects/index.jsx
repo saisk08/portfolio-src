25ff8b82494cd7dcaf11b298df8481953d93c5cf
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-03 18:35:44 +0530

    Remove graphql styles

--- src/components/landing/Projects/index.jsx
+++ src/components/landing/Projects/index.jsx
@@ -1,66 +1,121 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
 import { Container, Card } from 'components/common';
 import starIcon from 'assets/icons/star.svg';
 import forkIcon from 'assets/icons/fork.svg';
 import { Wrapper, Grid, Item, Content, Stats } from './styles';
 
-export const Projects = () => {
-  const {
-    github: {
-      viewer: {
-        repositories: { edges },
-      },
-    },
-  } = useStaticQuery(
-    graphql`
-      {
-        github {
-          viewer {
-            repositories(first: 8, orderBy: { field: STARGAZERS, direction: DESC }) {
-              edges {
-                node {
-                  id
-                  name
-                  url
-                  description
-                  stargazers {
-                    totalCount
-                  }
-                  forkCount
-                }
-              }
-            }
-          }
-        }
-      }
-    `
-  );
-  return (
-    <Wrapper as={Container} id="projects">
-      <h2>Projects</h2>
-      <Grid>
-        {edges.map(({ node }) => (
-          <Item key={node.id} as="a" href={node.url} target="_blank" rel="noopener noreferrer">
-            <Card>
-              <Content>
-                <h4>{node.name}</h4>
-                <p>{node.description}</p>
-              </Content>
-              <Stats>
-                <div>
-                  <img src={starIcon} alt="stars" />
-                  <span>{node.stargazers.totalCount}</span>
-                </div>
-                <div>
-                  <img src={forkIcon} alt="forks" />
-                  <span>{node.forkCount}</span>
-                </div>
-              </Stats>
-            </Card>
-          </Item>
-        ))}
-      </Grid>
-    </Wrapper>
-  );
-};
+export const Projects = () => (
+  <Wrapper as={Container} id="projects">
+    <h2>Projects</h2>
+    <Grid>
+      <Item key={0} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+      <Item key={1} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+      <Item key={2} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+      <Item key={3} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+      <Item key={4} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+      <Item key={5} as="a" href="#" target="_blank" rel="noopener noreferrer">
+        <Card>
+          <Content>
+            <h4>Test</h4>
+            <p>Test</p>
+          </Content>
+          <Stats>
+            <div>
+              <img src={starIcon} alt="stars" />
+              <span>{0}</span>
+            </div>
+            <div>
+              <img src={forkIcon} alt="forks" />
+              <span>{1}</span>
+            </div>
+          </Stats>
+        </Card>
+      </Item>
+    </Grid>
+  </Wrapper>
+);
