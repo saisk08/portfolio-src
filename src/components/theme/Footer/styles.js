efc4c41d2782b50a3f6096246cda0d4e090054a6
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-03 18:35:09 +0530

    Update text and styles

--- src/components/theme/Footer/styles.js
+++ src/components/theme/Footer/styles.js
@@ -1,16 +1,7 @@
-import footerIllustration from 'assets/illustrations/footer.svg';
 import styled from 'styled-components';
 
 export const Wrapper = styled.div`
-  padding: 28rem 0 4rem 0;
-  background-image: url(${footerIllustration});
-  background-size: cover;
-  background-position: top;
-  background-repeat: no-repeat;
-
-  @media (max-width: 1960px) {
-    padding: 14rem 0 4rem;
-  }
+  padding-top: 8rem;
 `;
 
 export const Flex = styled.div`
