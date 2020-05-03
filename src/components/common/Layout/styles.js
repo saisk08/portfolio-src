8aa28065cc29a115317deea942149a960af39e3d
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-03 18:33:27 +0530

    Remove footer

--- src/components/common/Layout/styles.js
+++ src/components/common/Layout/styles.js
@@ -2,7 +2,7 @@ import { createGlobalStyle } from 'styled-components';
 
 export const Global = createGlobalStyle`
   html {
-    font-family: 'Roboto', Helvetica, sans-serif;
+    font-family: 'Avenir', Helvetica, sans-serif;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
 
