9be8de207d1aa0c0a50f399a6d603916224b5eb2
Sai Somanath Komanduri <k.saisomanath@gmail.com>
2020-05-08 19:56:50 +0530

    Add svg in footer

--- src/components/theme/Footer/styles.js
+++ src/components/theme/Footer/styles.js
@@ -1,7 +1,16 @@
 import styled from 'styled-components';
+import footerIllustration from 'assets/illustrations/footer.svg';
 
 export const Wrapper = styled.div`
-  padding-top: 8rem;
+  padding: 28rem 0 4rem 0;
+  background-image: url(${footerIllustration});
+  // background-color: #00b0ff;
+  background-size: cover;
+  background-position: top;
+  background-repeat: no-repeat;
+  @media (max-width: 1960px) {
+    padding: 14rem 0 4rem;
+  }
 `;
 
 export const Flex = styled.div`
